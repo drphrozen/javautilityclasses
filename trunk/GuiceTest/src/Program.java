b4237f914e9e66590a30f785e90438870c55268e
drphrozen <drphrozen@b2ace81b-87d4-8bf7-a174-b444a8e1594b>
2010-09-08 16:54:28 +0000

    Added GuiceTest

---
+++ trunk/GuiceTest/src/Program.java
@@ -0,0 +1,22 @@
+
+
+public class Program {
+
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+interface Driver {
+	int getId();
+}
+
+//class Sensor implements Driver {
+//	RealDriver
+//}
